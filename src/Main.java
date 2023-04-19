fac2a500d3dced90d46e94db3b3c15270da871e9
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:59:17 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Animal animal = new Animal();
+        animal.speak();
+        dog.speak();
+    }
+}
